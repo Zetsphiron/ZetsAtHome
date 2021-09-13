d8bed457e6c37d24fd430cccb1c8dacd63651973
Zetsphiron <zataraincarlos@hotmail.com>
2021-09-13 10:35:32 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webpage</title>
+</head>
+<body>
+    <h1>
+        Welcome to this test
+    </h1>
+    Here's an image
+    <img src="./img/geocities.jpg" alt="heehoo", width="25%">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos maxime quae laborum impedit molestias nam vel neque qui animi. Quam autem molestias et exercitationem enim sit, fugiat doloremque vel ut?
+    <a href="https://zataraincarlos17.000webhostapp.com/">Page on 000webhost</a>
+    <a href="https://first.zetsphiron.repl.co/">Page on Replit.co</a>
+</body>
+</html>
