969d9e780cd231d3fe2ddb7582f01aff0a68c319
Zetsphiron <zataraincarlos@hotmail.com>
2021-09-13 22:24:06 -0700

    Forgot PHP

--- index.php
+++ index.php
@@ -1,19 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/brands.css">
+
     <title>Webpage</title>
 </head>
 <body>
     <h1>
-        Welcome to this test
+        Bienvenido a mi página!
     </h1>
-    Here's an image
-    <img src="./img/geocities.jpg" alt="heehoo", width="25%">
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos maxime quae laborum impedit molestias nam vel neque qui animi. Quam autem molestias et exercitationem enim sit, fugiat doloremque vel ut?
-    <a href="https://zataraincarlos17.000webhostapp.com/">Page on 000webhost</a>
-    <a href="https://first.zetsphiron.repl.co/">Page on Replit.co</a>
+    He aquí una imagen ;)
+    </br>
+    </br><img src="./img/geocities.jpg" alt="heehoo", width="400px">
+    </br>
+    </br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos maxime quae laborum impedit molestias nam vel neque qui animi. Quam autem molestias et exercitationem enim sit, fugiat doloremque vel ut?
+    </br>
+    <hr>
+
+    <table>
+        <tr>
+            <th><i class="fas fa-globe"></i></th>
+            <th><a href="https://zataraincarlos17.000webhostapp.com/">Página en 000webhost</a></th>
+        </tr>
+        <tr>
+            <th><i class="fas fa-robot"></i></th>
+            <th><a href="https://first.zetsphiron.repl.co/">Página en Replit.co</a></th>
+        </tr>
+        <tr>
+            <th><i class="fab fa-kaggle"></i></th>
+            <th><a href="https://pds2-page.herokuapp.com">Página en Heroku</a></th>
+        </tr>
+        <tr>
+            <th><i class="fas fa-paw"></i></th>
+            <th><a href="https://zetsphiron.neocities.org/">Página en NeoCities</a></th>
+        </tr>
+        <tr>
+            <th><i class="fab fa-github"></i></th>
+            <th><a href="https://zetsphiron.github.io/pds2-page-gh/">Página plantilla en GitHub</a></th>
+        </tr>
+    </table>
+
 </body>
 </html>
